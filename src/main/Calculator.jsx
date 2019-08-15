e691beb536d79d5c3740e99f3fe8a013dca4029b
Iury Christmas <iury.christmas@gmail.com>
2019-08-15 12:09:37 -0300

    implementada logica de calculo e finalizado o projeto calculadora

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,7 +4,18 @@ import './Calculator.css';
 import Button from '../components/Button';
 import Display from '../components/Display';
 
+
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
+
 export default class Calculator extends Component {
+
+    state = { ...initialState };
 
     constructor(props) {
         super(props);
@@ -14,22 +25,62 @@ export default class Calculator extends Component {
     } 
 
     clearMemory() {
-        console.log('limpar');
+        this.setState({ ...initialState });
     }
 
     setOperation(operation) {
-        console.log(operation);
+        if(this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true });
+            return;
+        }
+
+        const equals = operation === '=';
+        const currentOperation = this.state.operation;
+
+        const values = [ ...this.state.values];
+        try {
+            values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`);
+        } catch(e) {
+            values[0] = this.state.values[0];
+        }
+        values[1] = 0;
+
+        this.setState({
+            displayValue: values[0],
+            operation: equals ? null : operation,
+            current: equals ? 0 : 1,
+            clearDisplay: !equals,
+            values
+        })
     }
 
     addDigit(digit) {
-        console.log(digit);
+        if(digit === '.' && this.state.displayValue.includes('.')) {
+            return;
+        }
+
+        const clearDisplay = this.state.displayValue === '0'
+            || this.state.clearDisplay;
+        const currentValue = clearDisplay ? '' : this.state.displayValue;
+        const displayValue = currentValue + digit;
+        this.setState({ displayValue, clearDisplay: false });
+    
+        if( digit !== '.') {
+            const index = this.state.current;
+            const newValue = parseFloat(displayValue);
+            //clonou os valores usando o spret dentro de um novo array
+            const values = [...this.state.values];
+            values[index] = newValue;
+            //substitui o array a partir do novo array criado
+            this.setState({ values });
+        }
     }
 
     render () {
        
         return (
             <div className="calculator">
-                <Display value={0} />
+                <Display value={this.state.displayValue} />
                 <Button label="AC" click={this.clearMemory} triple />
                 <Button label="/" click={this.setOperation} operation />
                 <Button label="7" click={this.addDigit} />
